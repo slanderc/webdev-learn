45e82766b2bd0b3dd7ab5b8d4bda66b38aa61c89
slanderc <Vilyge1231@gmail.con>
2017-10-31 20:24:59 +0300

    Add input

--- js/searchinarray.js
+++ js/searchinarray.js
@@ -6,3 +6,15 @@ var randomArray = [];
 for (var i = 0; i < SIZE_ARRAY; i++) {
   randomArray.push(Math.floor(Math.random() * MAX_RANDOM_BOARD));  
 }
+
+do {
+  var key = prompt('Enter element:', '');
+  var checkFlag = false;
+  if (key % 1 != 0) {
+    checkFlag = true;  
+  }
+  if (checkFlag) {
+    alert("Input error! Repeat input."); 
+  }
+} while (checkFlag || (key == '') || isNaN(key)
+         || (key == null));
