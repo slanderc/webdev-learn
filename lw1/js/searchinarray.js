024ffdfa4b8a113fb9fa5fe98a5d3af28f5c67a4
slanderc <Vilyge1231@gmail.con>
2017-11-10 10:38:43 +0300

    Rename variable

--- lw1/js/searchinarray.js
+++ lw1/js/searchinarray.js
@@ -8,21 +8,21 @@ for (var i = 0; i < SIZE_ARRAY; i++) {
 }
 
 do {
-  var key = prompt('Enter element:', '');
+  var keySearch = prompt('Enter element:', '');
   var checkFlag = false;
-  if (key % 1 != 0) {
+  if (keySearch % 1 != 0) {
     checkFlag = true; 
   }
   if (checkFlag) {
     alert("Input error! Repeat input."); 
   }
-} while (checkFlag || (key == '') || isNaN(key));
+} while (checkFlag || (keySearch == '') || isNaN(keySearch));
 
-if (key != null) {
+if (keySearch != null) {
   document.write("<p>Filled random array:</p>");
   document.write("<p>" + randomArray + "</p>");
        
-  if (randomArray.indexOf(parseInt(key)) !== -1) {
+  if (randomArray.indexOf(parseInt(keySearch)) !== -1) {
     document.write("<p>This element in random array!</p>"); 
   } else {
     document.write("<p>This element not in random array.</p>");
