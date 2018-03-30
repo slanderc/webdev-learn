648541afbbcb07518ca20c329c2aafc2eac6ad57
slanderc <ya.wexwexquas@gmail.com>
2018-03-30 21:11:09 +0300

    Create checkCash test

--- lw7/tests/tests.js
+++ lw7/tests/tests.js
@@ -11,3 +11,34 @@ describe('Метод getCoffeeMenu', function() {
     });
   });
 });
+
+describe('Метод checkCash', function() {
+  function makeTest(value, expected) {
+    it(`принимает на вход ${value} и должен вернуть ${expected}`, function() {
+      let test = new CoffeeMachine();
+      let actual = test.checkCash(value);
+      assert.equal(expected, actual);
+    });
+  }
+  let arranges = [
+    { value: null, expected: false },
+    { value: undefined, expected: false },
+    { value: -1, expected: false },
+    { value: -10, expected: false },
+    { value: 0, expected: false },
+    { value: 11, expected: false },
+    { value: 6, expected: false },
+    { value: 15, expected: false },
+    { value: 13, expected: false },
+    { value: 9, expected: false },
+    { value: 1, expected: true },
+    { value: 2, expected: true },
+    { value: 5, expected: true },
+    { value: 10, expected: true },
+    { value: 50, expected: true },
+    { value: 100, expected: true }
+  ];
+  arranges.forEach(function(arrange, i) {
+    makeTest(arrange.value, arrange.expected);
+  });
+});
